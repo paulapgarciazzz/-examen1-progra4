9524e8816f38510b2f4b2d759237190e044230a4
Maria Paula Pérez Garcia <paulapgarciazzz@gmail.com>
2026-04-23 19:13:06 -0600

    configure app routes

---
+++ Examen1-Progra4/src/components/CarParts.jsx
@@ -0,0 +1,8 @@
+const CarParts = () => {
+    return(
+        <div className="carparts">
+            <h1>Repuestos </h1> 
+        </div>
+    )
+}
+export default CarParts;
---
+++ Examen1-Progra4/src/components/routes.jsx
@@ -0,0 +1,40 @@
+import{
+    createRootRoute,
+    createRoute,
+    createRouter,
+    Link,
+    Outlet
+}from '@tanstack/react-router';
+
+import Home from './Home';
+import CarParts from './CarParts';
+import './App.css';
+
+const rootRoute = createRootRoute({
+    component: function RootLayout(){
+        return(
+            <>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/carparts">Repuestos</Link>
+            </nav>
+
+            <section id="center">
+                <Outlet />
+            </section>
+            </>
+        )
+    }
+});
+const homeRoute = createRoute({
+    getParentRoute: () => rootRoute,
+    path: "/",
+    component: Home
+});
+const repuestosRoute = createRoute({
+    getParentRoute: () => rootRoute,
+    path: "/carparts",
+    component: CarParts
+});
+const routeTree = rootRoute.addChildren([homeRoute, repuestosRoute]);
+export const router = createRouter(routeTree);
